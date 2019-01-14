7e276824255f3312f309c020d6f3a1c381d51092
Tres Reid <tres.reid@gmail.com>
2019-01-14 16:53:27 -0500

    decent results

--- second_attempt/helix_getmeas_test5.py
+++ second_attempt/helix_getmeas_test5.py
@@ -7,17 +7,18 @@ init_point = array([0,0,0])
 del_x = 10
 truth_adjust = array([del_x,randint(-10,10),randint(-10,10)])
 #truth_adjust = array([del_x,3,0])
-num_steps = 100
+num_steps = 1000
 #res =5.0
 #z_res = 1
 #rad = 1./BM # mv/qB = k/B let k=1
 #c = 3.0*10**(8)
 #BM = 10./c
-a = 100.#1/(c*BM)
+a = 500.#1/(c*BM)
 k_0=1.
 rad = a/k_0#1./BM # mv/qB = k/B let k=1
 res = 10
-res_max = 10.
+res_max = 20
+ang_steps = 2*pi/(1*num_steps)
 def make_truth(init_point,truth_adjust):
 	truth_datax =[]
 	truth_datay =[]
@@ -27,8 +28,8 @@ def make_truth(init_point,truth_adjust):
 	for t_step in range(num_steps):
 		#t=1
 		#new_point = new_point + dot(t,truth_adjust)
-		truth_datax.append(rad*(cos((2*pi*t_step/(2*num_steps))) ))
-		truth_datay.append(rad*sin((2*pi*t_step/(2*num_steps))))
+		truth_datax.append(rad*(cos((t_step*ang_steps))))
+		truth_datay.append(rad*(sin(t_step*ang_steps)))
 		truth_dataz.append(10*t_step)
 	return (truth_datax,truth_datay,truth_dataz)
 truth_x, truth_y,truth_z = make_truth(init_point,truth_adjust)
@@ -57,27 +58,29 @@ def make_coords(state,phix,meas):
 	## I feel confident about this function. only issue is when dp (state[0]) is far from 0
 	#state[0] = 0
 	#state[3] = 0
-	if state[1]<0:
-		sign = -1
-	else:
-		sign = 1
+#	if state[1]>pi:#0:
+#		sign = -1
+#	else:
+	sign = 1
 	x_coord = sign*(state[0]*cos(state[1]) + (a/(state[2]))*(cos(state[1])))
 	y_coord = sign*(state[0]*(sin(state[1])) + (a/(state[2]))*(sin(state[1])))
+	#z_coord = meas[2] + state[3] + (a/(state[2]))*(state[4])*phix
 	z_coord = meas[2] + state[3] + (a/(state[2]))*(state[4])*phix
 	return array([x_coord,y_coord,z_coord])
 def convert_meas(meas0,meas1,xc,z=False,step=0):
 	###still have issues with X_c and Y_c. Also dp does not always give good values for truth
-#	X_c = meas0[0] + (xc[0]+a/xc[2])*cos(xc[1])
-#	Y_c = meas0[1] + (xc[0]+a/xc[2])*sin(xc[1])
+#	X_c = meas0[0] + (xc[0]-a/xc[2])*abs(cos(xc[1]))
+#	Y_c = meas0[1] + (xc[0]-a/xc[2])*abs(sin(xc[1]))
 	X_c =0 
 	Y_c =0 
-	
+#	print "X_c: ", X_c
+#	print "Y_c: ",Y_c	
 	m_phi = atan((meas1[1]-Y_c)/(meas1[0]-X_c))
-	m_phi1 = atan((meas1[1]-Y_c)/(meas1[0]-X_c))
-	adj = -1
-	if m_phi < 0:
-		adj = 1	
-	m_dp = (X_c-meas1[0])*cos(m_phi1) + (Y_c-meas1[1])*sin(m_phi1) - adj*(a/xc[2])
+	if sign(meas1[0]) <0: # only if x is negative add pi
+		m_phi = m_phi +pi
+	if sign(meas1[0]) >0 and sign(meas1[1])<0:
+		m_phi = m_phi +2*pi
+	m_dp = (X_c-meas1[0])*cos(m_phi) + (Y_c-meas1[1])*sin(m_phi) + (a/xc[2])
 	m_k = xc[2]
 	m_tanl = xc[4]
 	m_dz = (meas0[2]-meas1[2])+((a/xc[2])*(m_phi-xc[1])*m_tanl) + xc[3]
@@ -98,7 +101,7 @@ init_dp =sqrt( measured_x[0]**2 + measured_y[0]**2) - (a/k_0)
 current_state = array([init_dp,init_azmu,k_0,init_dz,init_dip])
 current_un = array([[1,0,0,0,0],[0,1,0,0,0],[0,0,1,0,0],[0,0,0,1,0],[0,0,0,0,1]])
 state_history = [current_state]
-state_history_coords = [array([100,0,0])]
+state_history_coords = [array([rad,0,0])]
 state_history_coords_true = [array([truth_x[0],truth_y[0],truth_z[0]])]
 state_history_coords_meas = [array([measured_x[0],measured_y[0],measured_z[0]])]
 state_history_true = [array([0,0,1,0,init_dip])]
@@ -115,16 +118,20 @@ R = array([[1,0,0,0,0],[0,pi/100,0,0,0],[0,0,.001,0,0],[0,0,0,1,0],[0,0,0,0,.1]]
 #	y_coord = meas[1] + state[0]*abs(sin(state[1])) - (a/(state[2]))*(sin(state[1])-sin(state[1]+phix))
 #	z_coord = meas[2] + state[3] + (a/(state[2]))*(state[4])*phix
 #	return array([x_coord,y_coord,z_coord])
-	
-def predict_step(x_cur,p_cur,step,del_phi,dak0,dak1,k,tanl):
-	
-	F = array([ 
-	[cos(del_phi),dak0*sin(del_phi),(a/(k*k))*(1-cos(del_phi)),0,0], #d dP/da
+def make_prediction_matrix(del_phi,dak0,dak1,k,tanl):
+	F_matrix = array([ 
+	[cos(del_phi),0*dak1*sin(del_phi),0*(a/(k*k))*(1-cos(del_phi)),0,0], #d dP/da
+	#[1,0,0,0,0],
+	#[0,0,(a/(k*k))*(1-cos(del_phi)),0,0], #d dP/da
 	[-(1/dak1)*sin(del_phi),(dak0/dak1)*cos(del_phi),(a/(k*k*(dak1)))*sin(del_phi),0,0], # dphi/da
 	[0,0,1,0,0],#dk/da
 	[(a/(k*dak1))*tanl*sin(del_phi),(a/k)*tanl*(1-(dak0/dak1)*cos(del_phi)),(a/(k*k))*tanl*(del_phi-(a/(k*dak1))*sin(del_phi)),1,-del_phi*(a/k)], #ddz/da
 	[0,0,0,0,1] #dtanl/da
 	])
+	return F_matrix
+	
+def predict_step(x_cur,p_cur,step,del_phi,dak0,dak1,k,tanl):
+	F = make_prediction_matrix(del_phi,dak0,dak1,k,tanl)
 	B = array([0,0,0,0,0])#(x_cur[4]**3)/3])
 	suppression = 1000
 	Q = suppression*array([
@@ -175,8 +182,13 @@ def update_step(step,x_cur,p_cur):
 	dp1 = z_state[0]
 	phi1 = z_state[1]
 	dak1 = (dp1+a/k)
-	del_phi = phi1-phi0
-	
+	#del_phi = phi1-phi0
+	del_phi = ang_steps 
+	#if (phi1 <0 and phi0 > 0): 
+#		del_phi = phi1+pi - phi0
+#	elif (phi1>0 and phi0<0):
+#		del_phi = phi1-pi - phi0
+#	print "del_phi: ", del_phi	
 	x_pre, p_pre = predict_step(x_cur,p_cur,step, del_phi,dak0,dak1,k,tanl)
 	
 	K = dot(dot(p_pre,transpose(H)), linalg.inv(dot(dot(H,p_pre),transpose(H)) + R))
@@ -187,18 +199,35 @@ def update_step(step,x_cur,p_cur):
 	del_phi_z = z[1]-state_history_meas[-1][1]
 	tt_coords = make_coords(tt,del_phi_t,state_history_coords_true[-1])
 	z_coords = make_coords(z,del_phi_z,t0)#state_history_coords_meas[-1])
-	
 
-	#print "truth act: ",t1
-	#print "truth coords: ",tt_coords
-	#print "truth param: ",tt
+	t_phi0 = state_history_true[-1][1]
+	t_tanl = state_history_true[-1][4]
+	t_k = state_history_true[-1][2]
+	t_dp0 = state_history_true[-1][0]
+	t_dak0 = (t_dp0+a/t_k)
+	t_dp1 = tt[0]
+	t_phi1 = tt[1]
+	t_dak1 = (t_dp1+a/t_k)
+	t_del_phi = t_phi1-t_phi0
+	FF = make_prediction_matrix(t_del_phi,t_dak0,t_dak1,t_k,t_tanl)
+	#print "tt step %s: "%(step), tt
+#	print "F*tt: ", dot(FF,tt)
+#	print "Cos: ",cos(t_del_phi), t_dp1*cos(t_del_phi)
+#	print "sin: ",t_dak0*sin(t_del_phi), t_phi1*t_dak0*sin(t_del_phi)
+#	print "k: ",(1*a/(t_k*t_k))*(1-cos(t_del_phi))
+#	print "diff: ",dot(array([cos(t_del_phi),0*t_dak0*sin(t_del_phi),(1*a/(t_k*t_k))*(1-cos(t_del_phi)),0,0]),tt)
+
+#	print "truth act: ",t1
+#	print "truth coords: ",tt_coords
+#	print "truth param: ",tt
 	#print "meas act: ",z1
 	#print "meas coords: ",z_coords
-	print "meas param: ",z
-	print "kal param: ", x_new
-	#print "kal: ",make_coords(x_new,del_phi,z)#state_history_coords[step-1]))
+#	print "meas param: ",z
+#	print "kal param: ", x_new
+#	print "kal: ",make_coords(x_new,del_phi,z)#state_history_coords[step-1]))
 	state_history.append(x_new)
-	state_history_coords.append(make_coords(x_new,del_phi,z))#state_history_coords[step-1]))
+	state_history_coords.append(make_coords(x_new,del_phi,z0))#state_history_coords[step-1]))
+	#state_history_coords.append(make_coords(x_new,del_phi,state_history_coords[-1]))
 	state_history_true.append(tt)
 	state_history_meas.append(z)
 	state_history_coords_true.append(tt_coords)
@@ -215,6 +244,8 @@ for i,state in enumerate(state_history_coords):
 	kal_z.append(state[2])
 	#if i < 100:
 	#	print state_history[i]
+#	print "true: ",state_history_true[i]
+#	print "kal: ",state_history[i]
 ##############################################################
 fig=plt.figure()
 ax = fig.add_subplot(111,projection='3d')
@@ -234,11 +265,26 @@ plt.show()
 #ax.plot(truth_x,truth_y,truth_z,c='r')
 #plt.show()
 ax = plt.subplot(111)
-ax.set_xlabel("x")
+ax.set_xlabel("step")
 ax.set_ylabel("dis")
-#ax.plot(truth_x, [sqrt( (kx-tx)**2 + (ky-ty)**2 + (kz-tz)**2) for kx,ky,kz,tx,ty,tz in zip(kal_x,kal_y,kal_z,truth_x,truth_y,truth_z)],'r')
-#ax.plot(truth_x, [(kx-tx) for kx,tx in zip(kal_x,measured_x)],'m')
-#ax.plot(truth_x, [(ky-ty) for ky,ty in zip(kal_y,measured_y)],'b')
-#ax.plot(truth_x, [(kz-tz) for kz,tz in zip(kal_z,measured_z)],'g')
-#print [(kx-tx) for kx,tx in zip(kal_x,measured_x)]
+ax.plot(range(len(truth_x)), [sqrt( (kx-tx)**2 + (ky-ty)**2) for kx,ky,kz,tx,ty,tz in zip(kal_x,kal_y,kal_z,truth_x,truth_y,truth_z)],'g')
+ax.plot(range(len(truth_x)), [sqrt( (mx-tx)**2 + (my-ty)**2) for mx,my,mz,tx,ty,tz in zip(measured_x,measured_y,measured_z,truth_x,truth_y,truth_z)],'r')
+print "kal: ",  average([sqrt( (kx-tx)**2 + (ky-ty)**2) for kx,ky,kz,tx,ty,tz in zip(kal_x,kal_y,kal_z,truth_x,truth_y,truth_z)])
+print "meas: ", average([sqrt( (mx-tx)**2 + (my-ty)**2) for mx,my,mz,tx,ty,tz in zip(measured_x,measured_y,measured_z,truth_x,truth_y,truth_z)])
+plt.show()
+
+ax = plt.subplot(111)
+ax.set_xlabel("step")
+ax.set_ylabel("dis")
+ax.plot(range(len(truth_x)), [(kx-tx) for tx,kx in zip(truth_x,kal_x)],'g')
+ax.plot(range(len(truth_x)), [(ky-ty) for ty,ky in zip(truth_y,measured_y)],'r')
+plt.show()
+
+ax = plt.subplot(111)
+ax.set_xlabel("step")
+ax.set_ylabel("x")
+ax.plot(range(len(truth_x)), [kx for kx in kal_x],'g')
+ax.plot(range(len(truth_x)), [tx for tx in truth_x],'b')
+ax.plot(range(len(truth_x)), [ky for ky in kal_y],'m')
+ax.plot(range(len(truth_x)), [ty for ty in truth_y],'r')
 plt.show()
