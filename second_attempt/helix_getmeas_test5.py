afc72e0d7d9c3dc35f6bd3559684bb239260e1c1
Tres Reid <tres.reid@gmail.com>
2019-01-16 13:44:47 -0500

    working modle for x,y,z all together. from a>100 or so (max_res=30 and numsteps=1000)

--- second_attempt/helix_getmeas_test5.py
+++ second_attempt/helix_getmeas_test5.py
@@ -13,12 +13,24 @@ num_steps = 1000
 #rad = 1./BM # mv/qB = k/B let k=1
 #c = 3.0*10**(8)
 #BM = 10./c
-a = 500.#1/(c*BM)
+a = 1000.#1/(c*BM)
 k_0=1.
 rad = a/k_0#1./BM # mv/qB = k/B let k=1
 res = 10
-res_max = 20
-ang_steps = 2*pi/(1*num_steps)
+res_max = 30
+ang_steps = 2*pi/(1.2*num_steps)
+z_step = 10
+def get_angle(meas1):
+	m_phi = atan((meas1[1])/(meas1[0]))
+	if sign(meas1[0]) <0: # only if x is negative add pi
+		m_phi = m_phi +pi
+	if sign(meas1[0]) >0 and sign(meas1[1])<0:
+		m_phi = m_phi +2*pi
+	if meas1[2] > 10*5*(num_steps/6) and m_phi<pi/6:
+		m_phi = m_phi+2*pi
+	if meas1[2] < 300 and m_phi>(5*pi/6):
+		m_phi = m_phi-2*pi
+	return m_phi
 def make_truth(init_point,truth_adjust):
 	truth_datax =[]
 	truth_datay =[]
@@ -30,42 +42,54 @@ def make_truth(init_point,truth_adjust):
 		#new_point = new_point + dot(t,truth_adjust)
 		truth_datax.append(rad*(cos((t_step*ang_steps))))
 		truth_datay.append(rad*(sin(t_step*ang_steps)))
-		truth_dataz.append(10*t_step)
+		truth_dataz.append(z_step*t_step)
 	return (truth_datax,truth_datay,truth_dataz)
 truth_x, truth_y,truth_z = make_truth(init_point,truth_adjust)
 
 def make_measured(truth_x,truth_y,truth_z):
-	measured_x =[]
-	measured_y =[]
-	measured_z =[]
+	#measured_x =[]
+	#measured_y =[]
+	#measured_z =[]
+	measured_tuple =[]
 	for i,(x,y,z) in enumerate(zip(truth_x,truth_y,truth_z)):
 		if i%1 ==0:
 			m_x = x + (res_max/res)*randint(-res,res)
 			m_y = y + (res_max/res)*randint(-res,res)
 			m_z = z + (res_max/res)*randint(-res,res)
-#		if m_x**2 + m_y**2 + m_z**2 < res**2:
-#			continue
-			measured_x.append(m_x)
-			measured_y.append(m_y)
-			measured_z.append(m_z)
-	return measured_x,measured_y,measured_z
-measured_x,measured_y,measured_z = make_measured(truth_x,truth_y,truth_z)
-#def make_angle(x0,x1,z0,z1):
-	#if (z1-z0) ==0:
-	#	return 0
-	
-def make_coords(state,phix,meas):
-	## I feel confident about this function. only issue is when dp (state[0]) is far from 0
-	#state[0] = 0
-	#state[3] = 0
-#	if state[1]>pi:#0:
-#		sign = -1
-#	else:
-	sign = 1
-	x_coord = sign*(state[0]*cos(state[1]) + (a/(state[2]))*(cos(state[1])))
-	y_coord = sign*(state[0]*(sin(state[1])) + (a/(state[2]))*(sin(state[1])))
-	#z_coord = meas[2] + state[3] + (a/(state[2]))*(state[4])*phix
-	z_coord = meas[2] + state[3] + (a/(state[2]))*(state[4])*phix
+			measured_tuple.append((m_x,m_y,m_z))
+			#measured_x.append(m_x)
+			#measured_y.append(m_y)
+			#measured_z.append(m_z)
+	#print measured_tuple
+	#measured_sorted = measured_tuple
+	measured_sorted = sorted(measured_tuple, key=lambda x: x[2])
+	#measured_sorted = sorted(measured_tuple, key=lambda x: get_angle(x))
+	return measured_sorted
+	#return measured_x,measured_y,measured_z
+#measured_x,measured_y,measured_z = make_measured(truth_x,truth_y,truth_z)
+measured_all = make_measured(truth_x,truth_y,truth_z)
+measured_x =[]
+measured_y =[]
+measured_z =[]
+for meas_x,meas_y,meas_z in measured_all:
+	measured_x.append(meas_x)
+	measured_y.append(meas_y)
+	measured_z.append(meas_z)
+	#print meas_x,meas_y,meas_z
+def make_coords(state,phix,meas,z =False,dz0=0):
+	x_coord = state[0]*cos(state[1]) + (a/(state[2]))*(cos(state[1]))
+	y_coord = state[0]*(sin(state[1])) + (a/(state[2]))*(sin(state[1]))
+	z_coord = meas[2] + (a/(state[2]))*(state[4])*phix - state[3] #+dz0
+#	z_coord =  (a/(state[2]))*(state[4])*state[1] - state[3] #+dz0
+	if z:
+		phix = ang_steps
+		z_coord =  (a/(state[2]))*(state[4])*state[1] #- state[3] #+dz0
+		#z_coord = meas[2] + (a/(state[2]))*(state[4])*2*abs(sin(phix/2)) + state[3] -dz0
+		z_coord = meas[2] + (a/(state[2]))*(state[4])*phix - state[3] +dz0
+#		print "z0: ",meas[2]
+#		print "phi: ",phix
+#		print "tanl: ", state[4]
+#		print "dxy: ", (a/(state[2]))*phix
 	return array([x_coord,y_coord,z_coord])
 def convert_meas(meas0,meas1,xc,z=False,step=0):
 	###still have issues with X_c and Y_c. Also dp does not always give good values for truth
@@ -75,11 +99,12 @@ def convert_meas(meas0,meas1,xc,z=False,step=0):
 	Y_c =0 
 #	print "X_c: ", X_c
 #	print "Y_c: ",Y_c	
-	m_phi = atan((meas1[1]-Y_c)/(meas1[0]-X_c))
-	if sign(meas1[0]) <0: # only if x is negative add pi
-		m_phi = m_phi +pi
-	if sign(meas1[0]) >0 and sign(meas1[1])<0:
-		m_phi = m_phi +2*pi
+#	m_phi = atan((meas1[1]-Y_c)/(meas1[0]-X_c))
+#	if sign(meas1[0]) <0: # only if x is negative add pi
+#		m_phi = m_phi +pi
+#	if sign(meas1[0]) >0 and sign(meas1[1])<0:
+#		m_phi = m_phi +2*pi
+	m_phi = get_angle(meas1)
 	m_dp = (X_c-meas1[0])*cos(m_phi) + (Y_c-meas1[1])*sin(m_phi) + (a/xc[2])
 	m_k = xc[2]
 	m_tanl = xc[4]
@@ -88,15 +113,21 @@ def convert_meas(meas0,meas1,xc,z=False,step=0):
 		m_dz = truth_z[step]-measured_z[step]	
 		m_sgn=(sqrt((truth_x[step])**2+(truth_y[step])**2) - (sqrt((measured_x[step])**2+(measured_y[step])**2)))
 		m_dp =sqrt( measured_x[step]**2 + measured_y[step]**2) - (a/m_k) 
+		#m_tanl = (meas1[2]-meas0[2])/(sqrt((meas1[0]-meas0[0])**2+(meas1[1]-meas0[1])**2))
+		#m_tanl = (meas1[2]-0)/(sqrt((meas1[0]-0)**2+(meas1[1]-0)**2))
+		#m_tanl = (meas1[2]-0)/(m_phi*a/m_k)
+		m_tanl = (truth_z[step]-0)/(rad*ang_steps*step)
 	return array([m_dp,m_phi,m_k,m_dz,m_tanl])
 ##############################################################
 # make the state out of x, theta and m(slope)
-init_azmu = atan((truth_y[0])/(truth_x[0]))
-init_dip = (truth_z[1]-truth_z[0])/(sqrt((truth_x[1]-truth_x[0])**2+(truth_y[1]-truth_y[0])**2))
-init_dz = truth_z[1]-measured_z[1]
-#init_dp=(sqrt((truth_x[1]-truth_x[0])**2+(truth_y[1]-truth_y[0])**2) - (sqrt((measured_x[1]-measured_x[0])**2+(measured_y[1]-measured_y[0])**2)))
-#init_dp=sqrt((truth_x[0]-measured_x[0])**2+(truth_y[0]-measured_y[0])**2) #- (sqrt((measured_x[1]-measured_x[0])**2+(measured_y[1]-measured_y[0])**2)))
-init_dp =sqrt( measured_x[0]**2 + measured_y[0]**2) - (a/k_0) 
+#init_azmu = atan((truth_y[0])/(truth_x[0]))
+#init_dip = (truth_z[1]-truth_z[0])/(sqrt((truth_x[1]-truth_x[0])**2+(truth_y[1]-truth_y[0])**2))
+#init_dz = truth_z[1]-measured_z[1]
+#init_dp =sqrt( measured_x[0]**2 + measured_y[0]**2) - (a/k_0) 
+z0 = array([measured_x[0],measured_y[0],measured_z[0]])
+z1 = array([measured_x[1],measured_y[1],measured_z[1]])
+#print "meas: "
+init_dp,init_azmu,init_k,init_dz,init_dip = convert_meas(z0,z1,array([0,0,1,0,z_step/(2*pi*a/ang_steps)]),True,1)	
 
 current_state = array([init_dp,init_azmu,k_0,init_dz,init_dip])
 current_un = array([[1,0,0,0,0],[0,1,0,0,0],[0,0,1,0,0],[0,0,0,1,0],[0,0,0,0,1]])
@@ -108,19 +139,15 @@ state_history_true = [array([0,0,1,0,init_dip])]
 state_history_meas = [array([0,0,1,0,init_dip])]
 
 u = del_x
-#R = array([[res_max**2,0,0],[0,res_max**2,0],[0,0,res_max**2]])
-R = array([[1,0,0,0,0],[0,pi/100,0,0,0],[0,0,.001,0,0],[0,0,0,1,0],[0,0,0,0,.1]])
+R = array([[1,0,0,0,0],[0,pi/100,0,0,0],[0,0,.001,0,0],[0,0,0,1,0],[0,0,0,0,2]])
 #def make_coords(state,phix,meas):
-#	## I feel confident about this function. only issue is when dp (state[0]) is far from 0
-#	#state[0] = 0
-#	#state[3] = 0
 #	x_coord = meas[0] + state[0]*abs(cos(state[1])) - (a/(state[2]))*(cos(state[1])-cos(state[1]+phix))
 #	y_coord = meas[1] + state[0]*abs(sin(state[1])) - (a/(state[2]))*(sin(state[1])-sin(state[1]+phix))
 #	z_coord = meas[2] + state[3] + (a/(state[2]))*(state[4])*phix
 #	return array([x_coord,y_coord,z_coord])
 def make_prediction_matrix(del_phi,dak0,dak1,k,tanl):
 	F_matrix = array([ 
-	[cos(del_phi),0*dak1*sin(del_phi),0*(a/(k*k))*(1-cos(del_phi)),0,0], #d dP/da
+	[cos(del_phi),dak1*sin(del_phi),(a/(k*k))*(1-cos(del_phi)),0,0], #d dP/da
 	#[1,0,0,0,0],
 	#[0,0,(a/(k*k))*(1-cos(del_phi)),0,0], #d dP/da
 	[-(1/dak1)*sin(del_phi),(dak0/dak1)*cos(del_phi),(a/(k*k*(dak1)))*sin(del_phi),0,0], # dphi/da
@@ -133,6 +160,7 @@ def make_prediction_matrix(del_phi,dak0,dak1,k,tanl):
 def predict_step(x_cur,p_cur,step,del_phi,dak0,dak1,k,tanl):
 	F = make_prediction_matrix(del_phi,dak0,dak1,k,tanl)
 	B = array([0,0,0,0,0])#(x_cur[4]**3)/3])
+	
 	suppression = 1000
 	Q = suppression*array([
 	[0,0,0,0,0],
@@ -189,7 +217,7 @@ def update_step(step,x_cur,p_cur):
 #	elif (phi1>0 and phi0<0):
 #		del_phi = phi1-pi - phi0
 #	print "del_phi: ", del_phi	
-	x_pre, p_pre = predict_step(x_cur,p_cur,step, del_phi,dak0,dak1,k,tanl)
+	x_pre, p_pre = predict_step(x_cur,p_cur,step, del_phi,dak0,dak1,k,tanl)#(10./pi))#tanl)
 	
 	K = dot(dot(p_pre,transpose(H)), linalg.inv(dot(dot(H,p_pre),transpose(H)) + R))
 
@@ -198,7 +226,7 @@ def update_step(step,x_cur,p_cur):
 	del_phi_t = tt[1]-state_history_true[-1][1]
 	del_phi_z = z[1]-state_history_meas[-1][1]
 	tt_coords = make_coords(tt,del_phi_t,state_history_coords_true[-1])
-	z_coords = make_coords(z,del_phi_z,t0)#state_history_coords_meas[-1])
+	z_coords = make_coords(z,del_phi_z,z0,True,state_history_meas[-1][3])
 
 	t_phi0 = state_history_true[-1][1]
 	t_tanl = state_history_true[-1][4]
@@ -217,16 +245,20 @@ def update_step(step,x_cur,p_cur):
 #	print "k: ",(1*a/(t_k*t_k))*(1-cos(t_del_phi))
 #	print "diff: ",dot(array([cos(t_del_phi),0*t_dak0*sin(t_del_phi),(1*a/(t_k*t_k))*(1-cos(t_del_phi)),0,0]),tt)
 
-#	print "truth act: ",t1
-#	print "truth coords: ",tt_coords
-#	print "truth param: ",tt
+#	k_coords = make_coords(x_new,del_phi,state_history_coords[-1],True,state_history[-1][3])
+	k_coords = make_coords(x_new,del_phi,z0,True,state_history[-1][3])
+	
+	#print "truth act: ",t1
+	#print "truth coords: ",tt_coords
+	print "truth param: ",tt
 	#print "meas act: ",z1
 	#print "meas coords: ",z_coords
-#	print "meas param: ",z
-#	print "kal param: ", x_new
-#	print "kal: ",make_coords(x_new,del_phi,z)#state_history_coords[step-1]))
+	print "meas param: ",z
+	print "kal param: ", x_new
+	#print "kal: ", k_coords #make_coords(x_new,del_phi,z,True,state_history_coords[step-1][3]))
 	state_history.append(x_new)
-	state_history_coords.append(make_coords(x_new,del_phi,z0))#state_history_coords[step-1]))
+	state_history_coords.append(k_coords)#state_history_coords[step-1]))
+	#state_history_coords.append(make_coords(x_new,del_phi,z0))#state_history_coords[step-1]))
 	#state_history_coords.append(make_coords(x_new,del_phi,state_history_coords[-1]))
 	state_history_true.append(tt)
 	state_history_meas.append(z)
@@ -267,24 +299,33 @@ plt.show()
 ax = plt.subplot(111)
 ax.set_xlabel("step")
 ax.set_ylabel("dis")
-ax.plot(range(len(truth_x)), [sqrt( (kx-tx)**2 + (ky-ty)**2) for kx,ky,kz,tx,ty,tz in zip(kal_x,kal_y,kal_z,truth_x,truth_y,truth_z)],'g')
-ax.plot(range(len(truth_x)), [sqrt( (mx-tx)**2 + (my-ty)**2) for mx,my,mz,tx,ty,tz in zip(measured_x,measured_y,measured_z,truth_x,truth_y,truth_z)],'r')
-print "kal: ",  average([sqrt( (kx-tx)**2 + (ky-ty)**2) for kx,ky,kz,tx,ty,tz in zip(kal_x,kal_y,kal_z,truth_x,truth_y,truth_z)])
-print "meas: ", average([sqrt( (mx-tx)**2 + (my-ty)**2) for mx,my,mz,tx,ty,tz in zip(measured_x,measured_y,measured_z,truth_x,truth_y,truth_z)])
+ax.plot(range(len(truth_x)), [sqrt( (kx-tx)**2 + (ky-ty)**2 + (kz-tz)**2) for kx,ky,kz,tx,ty,tz in zip(kal_x,kal_y,kal_z,truth_x,truth_y,truth_z)],'g')
+ax.plot(range(len(truth_x)), [sqrt( (mx-tx)**2 + (my-ty)**2 + (mz-tz)**2) for mx,my,mz,tx,ty,tz in zip(measured_x,measured_y,measured_z,truth_x,truth_y,truth_z)],'r')
+print "kal: ",  average([sqrt( (kx-tx)**2 + (ky-ty)**2 + (kz-tz)**2) for kx,ky,kz,tx,ty,tz in zip(kal_x,kal_y,kal_z,truth_x,truth_y,truth_z)])
+print "meas: ", average([sqrt( (mx-tx)**2 + (my-ty)**2 + (mz-tz)**2) for mx,my,mz,tx,ty,tz in zip(measured_x,measured_y,measured_z,truth_x,truth_y,truth_z)])
+#ax.plot(range(len(truth_x)), [sqrt( (kx-tx)**2 + (ky-ty)**2) for kx,ky,kz,tx,ty,tz in zip(kal_x,kal_y,kal_z,truth_x,truth_y,truth_z)],'g')
+#ax.plot(range(len(truth_x)), [sqrt( (mx-tx)**2 + (my-ty)**2) for mx,my,mz,tx,ty,tz in zip(measured_x,measured_y,measured_z,truth_x,truth_y,truth_z)],'r')
+#print "kal: ",  average([sqrt( (kx-tx)**2 + (ky-ty)**2) for kx,ky,kz,tx,ty,tz in zip(kal_x,kal_y,kal_z,truth_x,truth_y,truth_z)])
+#print "meas: ", average([sqrt( (mx-tx)**2 + (my-ty)**2) for mx,my,mz,tx,ty,tz in zip(measured_x,measured_y,measured_z,truth_x,truth_y,truth_z)])
 plt.show()
 
 ax = plt.subplot(111)
 ax.set_xlabel("step")
 ax.set_ylabel("dis")
-ax.plot(range(len(truth_x)), [(kx-tx) for tx,kx in zip(truth_x,kal_x)],'g')
-ax.plot(range(len(truth_x)), [(ky-ty) for ty,ky in zip(truth_y,measured_y)],'r')
+#ax.plot(range(len(truth_x)), [(kx-tx) for tx,kx in zip(truth_x,kal_x)],'g')
+#ax.plot(range(len(truth_x)), [(ky-ty) for ty,ky in zip(truth_y,kal_y)],'r')
+ax.plot(range(len(truth_x)), [(kz-tz) for tz,kz in zip(truth_z,kal_z)],'g')
+ax.plot(range(len(truth_x)), [(mz-tz) for tz,mz in zip(truth_z,measured_z)],'r')
 plt.show()
 
 ax = plt.subplot(111)
 ax.set_xlabel("step")
 ax.set_ylabel("x")
-ax.plot(range(len(truth_x)), [kx for kx in kal_x],'g')
-ax.plot(range(len(truth_x)), [tx for tx in truth_x],'b')
-ax.plot(range(len(truth_x)), [ky for ky in kal_y],'m')
-ax.plot(range(len(truth_x)), [ty for ty in truth_y],'r')
+#ax.plot(range(len(truth_x)), [kx for kx in kal_x],'g')
+#ax.plot(range(len(truth_x)), [tx for tx in truth_x],'b')
+#ax.plot(range(len(truth_x)), [ky for ky in kal_y],'m')
+#ax.plot(range(len(truth_x)), [ty for ty in truth_y],'r')
+ax.plot(range(len(truth_x)), kal_z,'g')
+ax.plot(range(len(truth_x)), measured_z,'r')
+ax.plot(range(len(truth_x)), truth_z,'b')
 plt.show()
